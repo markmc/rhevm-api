bca4bd00bfed4c251c0449b563fdf8c727296c8b
Eoghan Glynn <eglynn@redhat.com>
2010-04-22 13:14:23 +0100

    Fix package on PowerShell application class

--- powershell/ejb/src/main/java/com/redhat/rhevm/api/powershell/deploy/PowerShellApplication.java
+++ powershell/ejb/src/main/java/com/redhat/rhevm/api/powershell/deploy/PowerShellApplication.java
@@ -16,7 +16,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package com.redhat.rhevm.api.dummy.deploy;
+package com.redhat.rhevm.api.powershell.deploy;
 
 import java.util.HashSet;
 import java.util.Set;
