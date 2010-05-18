ffdc30f6e5377393a859aa9a3e26d357df853c5a
Mark McLoughlin <markmc@redhat.com>
2010-05-18 15:20:24 +0100

    First cut of powershell attachment unit test

---
+++ powershell/jaxrs/src/test/java/com/redhat/rhevm/api/powershell/resource/PowerShellAttachmentResourceTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright © 2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.redhat.rhevm.api.powershell.resource;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import com.redhat.rhevm.api.model.Attachment;
+import com.redhat.rhevm.api.model.StorageDomainStatus;
+
+import com.redhat.rhevm.api.powershell.util.PowerShellUtils;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.easymock.classextension.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.isA;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { PowerShellUtils.class })
+public class PowerShellAttachmentResourceTest extends Assert {
+
+    private static final String URI_ROOT = "http://localhost:8099";
+    private static final String DATA_CENTER_ID = "d12345";
+    private static final String STORAGE_DOMAIN_ID = "s98765";
+    private static final String DATA_CENTER_URI = URI_ROOT + "/datacenters/" + DATA_CENTER_ID;
+    private static final String STORAGE_DOMAIN_URI = URI_ROOT + "/storagedomains/" + STORAGE_DOMAIN_ID;
+    private static final String ATTACHMENT_URI = STORAGE_DOMAIN_URI + "/attachments/" + DATA_CENTER_ID;
+    private static final String DEACTIVATE_ACTION_URI = ATTACHMENT_URI + "/deactivate";
+    private static final String GET_COMMAND =
+        "get-storagedomain" + " -datacenterid " + DATA_CENTER_ID + " -storagedomainid " + STORAGE_DOMAIN_ID;
+    private static final String GET_RETURN =
+        "storagedomainid:" + STORAGE_DOMAIN_ID + "\n" +
+        "status: active\n" +
+        "sharedstatus: active\n" +
+        "domaintype: data (master)\n" +
+        "type: nfs\n" +
+        "nfspath: foo.bar:/blaa/and/butter\n";
+
+    private ArrayList<HashMap<String,String>> getReturnProps() {
+        ArrayList<HashMap<String,String>> parsedProps = new ArrayList<HashMap<String,String>>();
+        HashMap<String,String> props = new HashMap<String,String>();
+        props.put("storagedomainid", STORAGE_DOMAIN_ID);
+        props.put("status", "active");
+        props.put("sharedstatus", "active");
+        props.put("domaintype", "data (master)");
+        props.put("type", "nfs");
+        props.put("nfspath", "foo.bar:/blaa/and/butter");
+        parsedProps.add(props);
+        return parsedProps;
+    }
+
+    private PowerShellAttachmentResource resource;
+
+    @Before
+    public void setUp() {
+        resource = new PowerShellAttachmentResource(DATA_CENTER_ID, STORAGE_DOMAIN_ID);
+    }
+
+    @After
+    public void tearDown() {
+        verifyAll();
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        verifyAttachment(resource.get(setUpAttachmentExpectations()));
+    }
+
+    private UriInfo setUpAttachmentExpectations() throws Exception {
+        mockStatic(PowerShellUtils.class);
+        expect(PowerShellUtils.runCommand(GET_COMMAND)).andReturn(GET_RETURN);
+        expect(PowerShellUtils.parseProps(GET_RETURN)).andReturn(getReturnProps());
+        UriInfo uriInfo = createMock(UriInfo.class);
+        UriBuilder uriBuilder = createMock(UriBuilder.class);
+        expect(uriInfo.getRequestUriBuilder()).andReturn(uriBuilder).anyTimes();
+        expect(uriInfo.getBaseUriBuilder()).andReturn(uriBuilder).anyTimes();
+        expect(uriBuilder.clone()).andReturn(uriBuilder).anyTimes();
+        expect(uriBuilder.path(isA(String.class))).andReturn(uriBuilder).anyTimes();
+        expect(uriBuilder.build()).andReturn(new URI(ATTACHMENT_URI));
+        expect(uriBuilder.build()).andReturn(new URI(STORAGE_DOMAIN_URI));
+        expect(uriBuilder.build()).andReturn(new URI(DATA_CENTER_URI));
+        expect(uriBuilder.build()).andReturn(new URI(DEACTIVATE_ACTION_URI));
+        replayAll();
+
+        return uriInfo;
+    }
+
+    private void verifyAttachment(Attachment attachment) {
+        assertNotNull(attachment);
+        assertEquals(attachment.getId(), DATA_CENTER_ID);
+        assertEquals(attachment.getName(), DATA_CENTER_ID);
+        assertNotNull(attachment.getDataCenter());
+        assertEquals(attachment.getDataCenter().getId(), DATA_CENTER_ID);
+        assertNotNull(attachment.getStorageDomain());
+        assertEquals(attachment.getStorageDomain().getId(), STORAGE_DOMAIN_ID);
+        assertEquals(attachment.getStatus(), StorageDomainStatus.ACTIVE);
+        assert(attachment.isMaster());
+    }
+}
