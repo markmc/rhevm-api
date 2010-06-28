edc2194a89ffeb0bd751a21b17e92bea0f34d7c5
Mark McLoughlin <markmc@redhat.com>
2010-06-28 15:56:05 +0100

    Add PowerShellPool
    PowerShellPool allows the pre-launching of PowerShellCmd instances
    
    We launch powershell instances in threads and then get() blocks until
    an instance is available

---
+++ powershell/jaxrs/src/main/java/com/redhat/rhevm/api/powershell/util/PowerShellPool.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright © 2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.redhat.rhevm.api.powershell.util;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PowerShellPool {
+
+    private static final int DEFAULT_POOL_SIZE = 4;
+
+    private ExecutorService executor;
+
+    private BlockingQueue<PowerShellCmd> cmds = new LinkedBlockingQueue<PowerShellCmd>();
+
+    public PowerShellPool(ExecutorService executor, int initialSize) {
+        this.executor = executor;
+
+        for (int i = 0; i < initialSize; i++) {
+            executor.execute(new PowerShellLauncher());
+        }
+    }
+
+    public PowerShellPool(ExecutorService executor) {
+        this(executor, DEFAULT_POOL_SIZE);
+    }
+
+    public PowerShellCmd get() {
+        PowerShellCmd cmd;
+        while (true) {
+            try {
+                cmd = cmds.take();
+                break;
+            } catch (InterruptedException ex) {
+                // ignore and block again
+            }
+        }
+        executor.execute(new PowerShellLauncher());
+        return cmd;
+    }
+
+    public void add(PowerShellCmd cmd) {
+        cmds.offer(cmd);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+
+        PowerShellCmd cmd;
+        while ((cmd = cmds.poll()) != null) {
+            cmd.destroy();
+        }
+    }
+
+    private class PowerShellLauncher implements Runnable {
+        @Override public void run() {
+            PowerShellCmd cmd = new PowerShellCmd();
+            cmd.start();
+            PowerShellPool.this.add(cmd);
+        }
+    }
+}
---
+++ powershell/jaxrs/src/test/java/com/redhat/rhevm/api/powershell/util/PowerShellPoolTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.redhat.rhevm.api.powershell.util;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import static org.easymock.classextension.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.expectLastCall;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.expectNew;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PowerShellPool.class})
+public class PowerShellPoolTest extends Assert {
+
+    @After
+    public void tearDown() {
+        verifyAll();
+    }
+
+    @Test
+    public void test() throws Exception {
+        setupExpectations();
+
+        ControllableExecutor executor = new ControllableExecutor();
+
+        PowerShellPool pool = new PowerShellPool(executor, 10);
+
+        assertEquals(10, executor.taskCount());
+
+        for (int i = 0; i < 5; i++) {
+            executor.runNext();
+        }
+
+        assertEquals(5, executor.taskCount());
+
+        PowerShellCmd cmd = pool.get();
+        assertNotNull(cmd);
+
+        assertEquals(6, executor.taskCount());
+
+        for (int i = 0; i < 6; i++) {
+            executor.runNext();
+        }
+
+        assertEquals(0, executor.taskCount());
+
+        pool.shutdown();
+    }
+
+    private void setupExpectations() throws Exception {
+        PowerShellCmd cmd = createMock(PowerShellCmd.class);
+        expectNew(PowerShellCmd.class).andReturn(cmd).anyTimes();
+        cmd.start();
+        expectLastCall().anyTimes();
+        cmd.destroy();
+        expectLastCall().anyTimes();
+        replayAll();
+    }
+}
