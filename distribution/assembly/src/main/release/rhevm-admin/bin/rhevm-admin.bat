c671fbac091bed97a1652c8e4cc7487e83c71d36
Eoghan Glynn <eglynn@redhat.com>
2010-07-01 12:25:53 +0100

    Fixed syntax error in rhevm-admin.bat

--- distribution/assembly/src/main/release/rhevm-admin/bin/rhevm-admin.bat
+++ distribution/assembly/src/main/release/rhevm-admin/bin/rhevm-admin.bat
@@ -11,7 +11,9 @@ if "%RHEVM_API_URL%" == "" (
   set RHEVM_API_URL=http://localhost:8080/rhevm-api-powershell
 )
 
-if "%JAVA_OPTS%" == "" set JAVA_OPTS="-Drhevm.base.url=%RHEVM_API_URL% -Drhevm.auth.username=%RHEVM_USERNAME% -Drhevm.auth.password=%RHEVM_PASSWORD%"
+if "%JAVA_OPTS%" == "" (
+  set JAVA_OPTS=-Drhevm.base.url=%RHEVM_API_URL% -Drhevm.auth.username=%RHEVM_USERNAME% -Drhevm.auth.password=%RHEVM_PASSWORD%
+)
 
 if "%SHIFT%" == "true" SET ARGS=%2 %3 %4 %5 %6 %7 %8
 if not "%SHIFT%" == "true" SET ARGS=%1 %2 %3 %4 %5 %6 %7 %8    
