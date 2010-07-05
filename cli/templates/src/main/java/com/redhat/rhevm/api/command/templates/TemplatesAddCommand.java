63cf1eec1df806143c69e4551acd153e7ebdccc2
Mark McLoughlin <markmc@redhat.com>
2010-07-05 12:50:33 +0100

    cli: add template add, remove and update commands

---
+++ cli/templates/src/main/java/com/redhat/rhevm/api/command/templates/TemplatesAddCommand.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright © 2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.redhat.rhevm.api.command.templates;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
+
+import com.redhat.rhevm.api.command.base.AbstractAddCommand;
+import com.redhat.rhevm.api.model.BootDevice;
+import com.redhat.rhevm.api.model.Cluster;
+import com.redhat.rhevm.api.model.CPU;
+import com.redhat.rhevm.api.model.CpuTopology;
+import com.redhat.rhevm.api.model.OperatingSystem;
+import com.redhat.rhevm.api.model.Template;
+import com.redhat.rhevm.api.model.VM;
+
+/**
+ * Add a new Template.
+ */
+@Command(scope = "templates", name = "add", description = "Add a new Virtual Machine Template")
+public class TemplatesAddCommand extends AbstractAddCommand<Template> {
+
+    @Argument(index = 0, name = "name", description = "Name of the template to add", required = true, multiValued = false)
+    protected String name;
+
+    @Option(name = "-v", aliases = { "--vm" }, description = "VM name", required = true, multiValued = false)
+    private String vm;
+
+    @Option(name = "-c", aliases = { "--cluster" }, description = "Cluster name", required = true, multiValued = false)
+    private String cluster;
+
+    @Option(name = "-m", aliases = { "--memory" }, description = "Memory size in Mb", required = false, multiValued = false)
+    private int memory = -1;
+
+    @Option(name = "-s", aliases = { "--sockets" }, description = "Number of sockets", required = false, multiValued = false)
+    private int sockets = -1;
+
+    @Option(name = "-r", aliases = { "--cores" }, description = "Number of CPUs per socket", required = false, multiValued = false)
+    private int cores = -1;
+
+    @Option(name = "-b", aliases = { "--boot" }, description = "Default boot sequence", required = false, multiValued = false)
+    private String boot;
+
+    protected Object doExecute() throws Exception {
+        display(doAdd(getModel(), Template.class, "templates", "template"));
+        return null;
+    }
+
+    protected Template getModel() {
+        Template model = new Template();
+        model.setName(name);
+        model.setVm(new VM());
+        model.getVm().setName(vm);
+        model.setCluster(new Cluster());
+        model.getCluster().setName(cluster);
+        if (memory != -1) {
+            model.setMemory(memory * 1024 * 1024L);
+        }
+        if (sockets != -1 || cores != -1) {
+            model.setCpu(new CPU());
+            model.getCpu().setTopology(new CpuTopology());
+            if (sockets != -1) {
+                model.getCpu().getTopology().setSockets(sockets);
+            }
+            if (cores != -1) {
+                model.getCpu().getTopology().setCores(cores);
+            }
+        }
+        if (boot != null) {
+            model.setOs(getOs());
+        }
+        return model;
+    }
+
+    private OperatingSystem getOs() {
+        OperatingSystem os = new OperatingSystem();
+        for (int i = 0; i < boot.length(); i++) {
+            char c = boot.charAt(i);
+            OperatingSystem.Boot boot = new OperatingSystem.Boot();
+
+            switch (c) {
+            case 'C':
+                boot.setDev(BootDevice.HD);
+                break;
+            case 'D':
+                boot.setDev(BootDevice.CDROM);
+                break;
+            case 'N':
+                boot.setDev(BootDevice.NETWORK);
+                break;
+            default:
+                break;
+            }
+
+            if (boot.isSetDev()) {
+                os.getBoot().add(boot);
+            }
+        }
+        return os;
+    }
+}
