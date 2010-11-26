8574934638fd6c8e52903219e295fdb85267b451
Eoghan Glynn <eglynn@redhat.com>
2010-11-26 14:30:57 +0000

    powershell: fixing PowerShellParser.PowerManagement.setUserName()

--- powershell/jaxrs/src/main/java/com/redhat/rhevm/api/powershell/util/PowerShellParser.java
+++ powershell/jaxrs/src/main/java/com/redhat/rhevm/api/powershell/util/PowerShellParser.java
@@ -372,7 +372,7 @@ public class PowerShellParser {
         public String getUsername() {
             return username;
         }
-        public void setUsername(String user) {
+        public void setUsername(String username) {
             this.username = username;
         }
 
