fc0adc84db856ee9de87aeb8070598dac33ac7d5
Mark McLoughlin <markmc@redhat.com>
2010-12-05 17:05:01 +0000

    Custom jackson JSON serializer
    Add a custom ObjectMapper so that we can exclude bogus "setFoo"
    properties, exclude null values and pretty print the output.

---
+++ api/src/main/java/com/redhat/rhevm/api/resteasy/CustomObjectMapper.java
@@ -0,0 +1,32 @@
+package com.redhat.rhevm.api.resteasy;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.ser.CustomSerializerFactory;
+
+public class CustomObjectMapper extends ObjectMapper
+{
+    public static CustomObjectMapper get() {
+        CustomObjectMapper mapper = new CustomObjectMapper();
+        mapper.setSerializer(new CustomBeanFactory()).includeDefaults(false).indent(true);
+        return mapper;
+    }
+
+    protected CustomObjectMapper setSerializer(CustomSerializerFactory ser) {
+        setSerializerFactory(ser);
+        getSerializationConfig().setSerializationView(String.class);
+        return this;
+    }
+
+    protected CustomObjectMapper includeDefaults(boolean include) {
+        getSerializationConfig().setSerializationInclusion(
+            include ? JsonSerialize.Inclusion.ALWAYS : JsonSerialize.Inclusion.NON_DEFAULT);
+        return this;
+    }
+
+    protected CustomObjectMapper indent(boolean indent) {
+        configure(SerializationConfig.Feature.INDENT_OUTPUT, indent);
+        return this;
+    }
+}
