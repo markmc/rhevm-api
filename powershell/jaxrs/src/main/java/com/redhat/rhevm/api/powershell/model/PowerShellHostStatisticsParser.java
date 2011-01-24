f5c43452c90dab58426758e21e70097c2fe37ec5
Eoghan Glynn <eglynn@redhat.com>
2011-01-24 21:07:25 +0000

    powershell: parse Host statistics independent of Host representation
    Allow the Host statistics to be parsed in isolation from the Hots itself.
    
    Acked-by: Mark McLoughlin <markmc@redhat.com>

---
+++ powershell/jaxrs/src/main/java/com/redhat/rhevm/api/powershell/model/PowerShellHostStatisticsParser.java
@@ -0,0 +1,161 @@
+package com.redhat.rhevm.api.powershell.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.redhat.rhevm.api.model.Statistic;
+import com.redhat.rhevm.api.model.StatisticType;
+import com.redhat.rhevm.api.model.StatisticUnit;
+import com.redhat.rhevm.api.model.ValueType;
+import com.redhat.rhevm.api.powershell.util.PowerShellParser;
+
+public class PowerShellHostStatisticsParser extends AbstractStatisticsParser {
+
+    private static final String MEMORY_STATS_TYPE = "RhevmCmd.CLIMemory";
+    private static final String CPU_STATS_TYPE = "RhevmCmd.CLICpu";
+    private static final String KSM_STATS_TYPE = "RhevmCmd.CLIKsm";
+
+    public static final String GET_HOST_STATS =
+        "$h = get-host {0} ; $h.getmemorystatistics() ; $h.getcpustatistics(); $h.getksmstatistics()";
+
+    static boolean isMemory(PowerShellParser.Entity entity) {
+        return MEMORY_STATS_TYPE.equals(entity.getType());
+    }
+
+    static boolean isCpu(PowerShellParser.Entity entity) {
+        return CPU_STATS_TYPE.equals(entity.getType());
+    }
+
+    static boolean isKsm(PowerShellParser.Entity entity) {
+        return KSM_STATS_TYPE.equals(entity.getType());
+    }
+
+    public static List<Statistic> parse(PowerShellParser parser, String output) {
+        List<Statistic> statistics = new ArrayList<Statistic>();
+
+        for (PowerShellParser.Entity entity : parser.parse(output)) {
+            if (isMemory(entity)) {
+                statistics.addAll(parseMemoryStats(entity));
+            } else if (isCpu(entity)) {
+                statistics.addAll(parseCpuStats(entity));
+            } else if (isKsm(entity)) {
+                statistics.addAll(parseKsmStats(entity));
+            }
+        }
+
+        return statistics;
+    }
+
+    static List<Statistic> parseMemoryStats(PowerShellParser.Entity entity) {
+        List<Statistic> statistics = new ArrayList<Statistic>();
+        Statistic statistic = null;
+
+        statistic = create("memory.installed",
+                           "Total physical memory",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        long mem = entity.get("physicalmemory", Integer.class)*Mb;
+        setDatum(statistic, mem);
+        statistics.add(statistic);
+
+        statistic = create("memory.total",
+                           "Total guest memory",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        setDatum(statistic, entity.get("commitedmemory", Integer.class)*Mb);
+        statistics.add(statistic);
+
+        statistic = create("memory.used",
+                           "Memory used",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        double memUsed = mem*entity.get("usagepercents", Integer.class)/100D;
+        setDatum(statistic, memUsed);
+        statistics.add(statistic);
+
+        statistic = create("memory.free",
+                           "Memory free",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        setDatum(statistic, mem-memUsed);
+        statistics.add(statistic);
+
+        statistic = create("swap.total",
+                           "Total swap",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        setDatum(statistic, entity.get("swaptotal", Long.class)*Mb);
+        statistics.add(statistic);
+
+        statistic = create("swap.free",
+                           "Free swap",
+                           StatisticType.GAUGE,
+                           StatisticUnit.BYTES,
+                           ValueType.INTEGER);
+        setDatum(statistic, entity.get("swapfree", Long.class)*Mb);
+        statistics.add(statistic);
+
+        return statistics;
+    }
+
+    static List<Statistic> parseCpuStats(PowerShellParser.Entity entity) {
+        List<Statistic> statistics = new ArrayList<Statistic>();
+        Statistic statistic = null;
+
+        statistic = create("cpu.current.user",
+                           "User+nic CPU usage",
+                           StatisticType.GAUGE,
+                           StatisticUnit.PERCENT,
+                           ValueType.DECIMAL);
+        setDatum(statistic, entity.get("user", BigDecimal.class).doubleValue());
+        statistics.add(statistic);
+
+        statistic = create("cpu.current.system",
+                           "System CPU usage",
+                           StatisticType.GAUGE,
+                           StatisticUnit.PERCENT,
+                           ValueType.DECIMAL);
+        setDatum(statistic, entity.get("system", BigDecimal.class).doubleValue());
+        statistics.add(statistic);
+
+        statistic = create("cpu.current.idle",
+                           "Idle CPU usage",
+                           StatisticType.GAUGE,
+                           StatisticUnit.PERCENT,
+                           ValueType.DECIMAL);
+        setDatum(statistic, entity.get("idle", BigDecimal.class).doubleValue());
+        statistics.add(statistic);
+
+        statistic = create("cpu.load.avg.5m",
+                           "5min CPU load average",
+                           StatisticType.GAUGE,
+                           StatisticUnit.NONE,
+                           ValueType.DECIMAL);
+        setDatum(statistic, entity.get("load", BigDecimal.class).doubleValue());
+        statistics.add(statistic);
+
+        return statistics;
+    }
+
+    static List<Statistic> parseKsmStats(PowerShellParser.Entity entity) {
+        List<Statistic> statistics = new ArrayList<Statistic>();
+        Statistic statistic = null;
+
+        statistic = create("ksm.cpu.current",
+                           "KSM CPU usage",
+                           StatisticType.GAUGE,
+                           StatisticUnit.PERCENT,
+                           ValueType.DECIMAL);
+        setDatum(statistic, entity.get("cpu", Integer.class));
+        statistics.add(statistic);
+
+        return statistics;
+    }
+
+}
---
+++ powershell/jaxrs/src/test/java/com/redhat/rhevm/api/powershell/model/PowerShellHostStatisticsParserTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright © 2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.redhat.rhevm.api.powershell.model;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.redhat.rhevm.api.model.Statistic;
+import com.redhat.rhevm.api.model.StatisticType;
+import com.redhat.rhevm.api.model.StatisticUnit;
+import com.redhat.rhevm.api.model.ValueType;
+
+public class PowerShellHostStatisticsParserTest extends PowerShellModelTest {
+
+    @Test
+    public void testParseStats() throws Exception {
+        String data = readFileContents("hostStats.xml");
+        assertNotNull(data);
+
+        List<Statistic> statistics = PowerShellHostStatisticsParser.parse(getParser(), data);
+        assertNotNull(statistics);
+        assertEquals(11, statistics.size());
+        verifyStatistic(statistics.get(0),
+                        "memory.installed",
+                        "Total physical memory",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        4096*1024*1024D);
+        verifyStatistic(statistics.get(1),
+                        "memory.total",
+                        "Total guest memory",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        1154*1024*1024D);
+        verifyStatistic(statistics.get(2),
+                        "memory.used",
+                        "Memory used",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        4096*1024*1024L/2D);
+        verifyStatistic(statistics.get(3),
+                        "memory.free",
+                        "Memory free",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        4096*1024*1024L/2D);
+        verifyStatistic(statistics.get(4),
+                        "swap.total",
+                        "Total swap",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        3955*1024*1024D);
+        verifyStatistic(statistics.get(5),
+                        "swap.free",
+                        "Free swap",
+                        StatisticType.GAUGE,
+                        StatisticUnit.BYTES,
+                        ValueType.INTEGER,
+                        2060*1024*1024D);
+        verifyStatistic(statistics.get(6),
+                        "cpu.current.user",
+                        "User+nic CPU usage",
+                        StatisticType.GAUGE,
+                        StatisticUnit.PERCENT,
+                        ValueType.DECIMAL,
+                        10D);
+        verifyStatistic(statistics.get(7),
+                        "cpu.current.system",
+                        "System CPU usage",
+                        StatisticType.GAUGE,
+                        StatisticUnit.PERCENT,
+                        ValueType.DECIMAL,
+                        20D);
+        verifyStatistic(statistics.get(8),
+                        "cpu.current.idle",
+                        "Idle CPU usage",
+                        StatisticType.GAUGE,
+                        StatisticUnit.PERCENT,
+                        ValueType.DECIMAL,
+                        30D);
+        verifyStatistic(statistics.get(9),
+                        "cpu.load.avg.5m",
+                        "5min CPU load average",
+                        StatisticType.GAUGE,
+                        StatisticUnit.NONE,
+                        ValueType.DECIMAL,
+                        2.1D);
+        verifyStatistic(statistics.get(10),
+                        "ksm.cpu.current",
+                        "KSM CPU usage",
+                        StatisticType.GAUGE,
+                        StatisticUnit.PERCENT,
+                        ValueType.DECIMAL,
+                        5D);
+    }
+
+    private void verifyStatistic(Statistic statistic,
+                                 String name,
+                                 String description,
+                                 StatisticType type,
+                                 StatisticUnit unit,
+                                 ValueType valueType,
+                                 double datum) {
+        assertEquals(name, statistic.getName());
+        assertEquals(description, statistic.getDescription());
+        assertEquals(type, statistic.getType());
+        assertEquals(unit, statistic.getUnit());
+        assertTrue(statistic.isSetValues());
+        assertEquals(valueType, statistic.getValues().getType());
+        assertTrue(statistic.getValues().isSetValues());
+        assertEquals(1, statistic.getValues().getValues().size());
+        assertEquals(datum, statistic.getValues().getValues().get(0).getDatum(), 0.1D);
+    }
+}
