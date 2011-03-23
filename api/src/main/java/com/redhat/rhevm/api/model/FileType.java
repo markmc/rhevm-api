1760a4b23589d0bdfcf2d4cb707b9f6df8c412e8
Mark McLoughlin <markmc@redhat.com>
2011-03-23 20:14:13 +0000

    api: make file types lowercase
    e.g.
    
      GET /rhevm-api/datacenters/123456/files HTTP/1.1
      Accept: application/xml
    
      HTTP/1.1 200 OK
      Content-Type: application/xml
    
      <files>
        <file id="foo.iso" href="/rhevm-api/datacenters/123456/files/foo.iso">
          <name>foo.iso</name>
          <type>iso</type>
          <data_center id="123456" href="/rhevm-api/datacenters/123456"/>
        </file>
        <file id="bar.vfd" href="/rhevm-api/datacenters/123456/files/bar.vfd">
          <name>bar.vfd</name>
          <type>vfd</type>
          <data_center id="123456" href="/rhevm-api/datacenters/123456"/>
        </file>
      </files>
    
    Acked-by: Geert Jansen <gjansen@redhat.com>

--- api/src/main/java/com/redhat/rhevm/api/model/FileType.java
+++ api/src/main/java/com/redhat/rhevm/api/model/FileType.java
@@ -22,12 +22,12 @@ public enum FileType {
     ISO, VFD;
 
     public String value() {
-        return name();
+        return name().toLowerCase();
     }
 
     public static FileType fromValue(String value) {
         try {
-            return valueOf(value);
+            return valueOf(value.toUpperCase());
         } catch (IllegalArgumentException e) {
             return null;
         }
